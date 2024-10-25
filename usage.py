a1216c6dd70257f0064f3eaa1378d96a0d5e73ba
Lauriane Jacot-Descombes <lauriane.jacot-descombes@bluewin.ch>
2024-10-25 12:43:00 +0000

    Add table example

--- usage.py
+++ usage.py
@@ -8,14 +8,20 @@ text = read_pdf(pdf_file)
 clean_text = xyz_processor(text, role="clean")
 save_xyz(text=clean_text, path_dir="examples/created_xyz/example_coords")
 
-# # Example with coordinates on several columns
-# pdf_file = "examples/pdf_files/example_columns.pdf"
-# text = read_pdf(pdf_file)
-# clean_text = xyz_processor(text, role="clean")
-# save_xyz(text=clean_text, path_dir="examples/created_xyz/example_columns")
+# Example with coordinates on several columns
+pdf_file = "examples/pdf_files/example_columns.pdf"
+text = read_pdf(pdf_file)
+clean_text = xyz_processor(text, role="clean")
+save_xyz(text=clean_text, path_dir="examples/created_xyz/example_columns")
 
-# # Example with columns and atomic numbers instead of element symbols
-# pdf_file = "examples/pdf_files/example_atomic_number.pdf"
-# text = read_pdf(pdf_file)
-# clean_text = xyz_processor(text, role="clean")
-# save_xyz(text=clean_text, path_dir="examples/created_xyz/example_atomic_number")
+# Example with columns and atomic numbers instead of element symbols
+pdf_file = "examples/pdf_files/example_atomic_number.pdf"
+text = read_pdf(pdf_file)
+clean_text = xyz_processor(text, role="clean")
+save_xyz(text=clean_text, path_dir="examples/created_xyz/example_atomic_number")
+
+# Example with columns, atomic numbers, and coordinates in table
+pdf_file = "examples/pdf_files/example_table.pdf"
+text = read_pdf(pdf_file)
+clean_text = xyz_processor(text, role="clean")
+save_xyz(text=clean_text, path_dir="examples/created_xyz/example_table")
